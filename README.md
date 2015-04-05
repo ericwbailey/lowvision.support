a6b5aceb5ba66036491c69be261c8aba0961a132
Eric Bailey <eric.w.bailey@gmail.com>
2015-04-05 19:36:00 -0400

    Mass commit

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
-# Low Vision Simulator
+# [lowvision.support](http://://lowvision.support/)
+Help improve your website's accessibility by simulating common low vision disorders and color-blindness.
 
-This is an attempt to simulate viewing the web with various low vision disabilities.
+
+## Motivation
+With the Section 508 refresh, This project arose from a desire to have 
+
+
+The simulation may not always be perfect, but should be close enough for evaluation purposes.
+
+Certain websites (<a rel="external" href="https://www.google.com/">Google</a>, <a rel="external" href="https://www.facebook.com/">Facebook</a>, <a rel="external" href="https://www.flickr.com/">Flickr</a>, etc.) do not allow embedding, so they will not be able to be evaluated. In order to retain interactivity with the submitted website, this simulator uses <abbr title="Scalable Vector Graphics">SVG</abbr> filter technology. Unfortunately, this means that only <a rel="external" href="http://caniuse.com/#feat=svg-filters">newer browsers</a> may be able to use this site effectively.
+
+## Known Issues
+- iFrame device density issues (will sometimes load desktop site)
+- iFrame obscures low vision condition section on iPhone (possibly related to device density issues above)
 
 
 ## Todo
-- [x] Add Font
+- [ ] Add support for:
+    - [] Hemianopia
+    - [] Glaucoma
+    - [] Macular Degeneration
+    - [] Retinitis Pigmentosa
+- [ ] Get instructions working
+- [ ] Add support for multiple low vision condition selections using [Chosen](https://harvesthq.github.io/chosen/) 
+- [ ] Add favicon info
 - [ ] Add Google Analytics
+
+
+## Contributors
+Help is more than welcome! Submit a pull request, or feel free to message me here on [Twitter](https://www.twitter.com/ericwbailey).
+
+## License
+A short snippet describing the license (MIT, Apache, etc.).
--- js/main-min.js
+++ js/main-min.js
@@ -1,3 +1,5 @@
+var siteTitle = 'lowvision.support';
+
 // Gets the URL from the Website URL field
 function getUrl(event) {
     var theUrl = $('#c-controls--url').val();
@@ -11,22 +13,43 @@ function getUrl(event) {
 
         // Hacky js nonsense that prepends http to the url if it's missing
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf#Description
-        if (theUrl.indexOf("http") == -1) {
-            theUrl = "http://" + theUrl;
+        if (theUrl.indexOf("http") === -1) {
+            theUrl = 'http://' + theUrl;
         }
-        $('#c-website-viewer--iframe').attr('src', theUrl);
 
+        // Loads the entered URL
+        $('#c-website-viewer').attr('src', theUrl);
+        document.title = siteTitle + ' - ' + theUrl.replace('http://', '');
+        console.log('Viewing ' + theUrl + '.');
+
+        // Removes initial disabled state of the low vision condition select menu
+        $('#c-controls--condition').removeAttr('disabled');
+
+        // Removes error state if valid URL is entered
+        $('[for="c-controls--url"]').removeClass('js-form-error--label');
+        $('#c-controls--url').removeClass('js-form-error--field');
+        $('[name="c-controls--submit"]').removeClass('js-form-error--button');
     }
+
     // Displays error state if the URL isn't valid
     else {
         $('[for="c-controls--url"]').addClass('js-form-error--label');
-        $('#c-controls--url').addClass('js-form-error--field').focus().select();
+        $('#c-controls--url').addClass('js-form-error--field').focus().select();    // Focus and select the text for easier fixing
         $('[name="c-controls--submit"]').addClass('js-form-error--button');
+        document.title = siteTitle + ' - Please use a valid URL';
+        console.log(theUrl + ' is not a valid URL!');
     }
 }
 
 
-// Loads the url when the submit button is pressed
+// Loads the URL when the submit button is pressed
 $('[name="c-controls--submit"]').click(getUrl);
+
+
+// Applies the selected condition
+$('#c-controls--condition').change(function() {
+    $('#c-website-viewer').attr('class', 'c-website-viewer' + ' ' + 'has-' + this.value.replace('option-', ''));
+    console.log('Applying condition ' + this.value.replace('option-', '') + '.');
+});
 
 
